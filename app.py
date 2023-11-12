a4a91a0236fa6ae0b3a912bd61ecd2daf82cd8a9
Crystal Luong <90412176+cwydle@users.noreply.github.com>
2023-11-12 00:03:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ ADMIN = [
 ]
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html', admin=ADMIN)
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
