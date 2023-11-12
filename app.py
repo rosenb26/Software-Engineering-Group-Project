447ccb1525890e84e07dbec9cb1fbd1883e3de64
Crystal Luong <90412176+cwydle@users.noreply.github.com>
2023-11-12 02:18:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,29 @@ ADMIN = [
     'email': 'tugrp@example.com',
     'username': 'adminusername',
     'password': 'adminpassword'
-    
+
   }
 ]
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/login")
+def login():
+  return render_template('login.html')
+
+
+@app.route("/register")
+def register():
+  return render_template('register.html')
+
+
+@app.route("/check")
+def check():
+  return render_template('check.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
