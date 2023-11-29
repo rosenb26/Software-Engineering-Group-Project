d50462d841d3ef7691d9bf0c429d8786c55045f0
rosenb26 <rosenb26@go.stockton.edu>
2023-11-29 11:57:46 -0500

    cleaned up database code

--- app/queries.py
+++ app/queries.py
@@ -1,71 +1,56 @@
 import sqlite3
 from werkzeug.security import check_password_hash
 
-def addResident(firstName, lastName, roomNumber):
-    connection = sqlite3.connect("springoak.db")
-    cursor = connection.cursor()
 
-    cursor.execute(
-        "INSERT INTO resident (FirstName, LastName, RoomNumber) VALUES (?, ?, ?)", (firstName, lastName, roomNumber)
-    )
-
-    connection.commit()
-    connection.close()
-
-def allRequests():
-    connection = sqlite3.connect("springoak.db")
-    cursor = connection.cursor()
-
-    cursor.execute(
-        "SELECT Description, Category, FirstName, LastName, RoomNumber FROM request JOIN resident ON request.resident = resident.ID;"
-    )
-    results = cursor.fetchall()
-    connection.close()
-    return results
-
-def allResidents():
-    connection = sqlite3.connect("springoak.db")
-    #connection.row_factory = sqlite3.Row
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT FirstName, LastName, RoomNumber FROM resident")
-    results = cursor.fetchall()
-    connection.close()
-    return results
-
-
-def allVisitors(checkedIn=True):
-    connection = sqlite3.connect("springoak.db")
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT FirstName, LastName, Resident FROM visitor")
-    results = cursor.fetchall()
-    connection.close()
-    return results
-
-
-def visitorInDatabase(firstName, lastName, email):
-    connection = sqlite3.connect("springoak.db")
-    cursor = connection.cursor()
-
-    cursor.execute(
-        "SELECT * FROM visitor WHERE FirstName = ? AND LastName = ? AND Email = ?",
-        (firstName, lastName, email),
-    )
-    result = cursor.fetchall()
-    return len(result) != 0
-    connection = sqlite3.connect("springoak.db")
-
-
-def checkStaffCredentials(username, password):
+def queryTheDabase(query, args=None, fetchOne=False):
     connection = sqlite3.connect("springoak.db")
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
 
-    cursor.execute(
-        "SELECT Username, PasswordHash from STAFF WHERE username = ?", (username,)
+    if args is None:
+        cursor.execute(query)
+    else:
+        cursor.execute(query, args)
+    results = cursor.fetchone() if fetchOne else cursor.fetchall()
+    connection.commit()
+    connection.close()
+    print(results)
+    return results
+
+
+def addResident(firstName, lastName, roomNumber):
+    queryTheDabase(
+        "INSERT INTO resident (FirstName, LastName, RoomNumber) VALUES (?, ?, ?)",
+        (firstName, lastName, roomNumber),
     )
-    result = cursor.fetchone()
+
+
+def allRequests():
+    return queryTheDabase(
+        "SELECT Description, Category, FirstName, LastName, RoomNumber FROM request JOIN resident ON request.resident = resident.ID;"
+    )
+
+
+def allResidents():
+    return queryTheDabase("SELECT FirstName, LastName, RoomNumber FROM resident")
+
+
+def allVisitors(checkedIn=True):
+    return queryTheDabase("SELECT FirstName, LastName, Resident FROM visitor")
+
+
+def visitorInDatabase(firstName, lastName, email):
+    result = queryTheDabase(
+        "SELECT * FROM visitor WHERE FirstName = ? AND LastName = ? AND Email = ?",
+        (firstName, lastName, email),
+    )
+    return len(result) != 0
+
+
+def checkStaffCredentials(username, password):
+    result = queryTheDabase(
+        "SELECT Username, PasswordHash from STAFF WHERE username = ?", (username,), True
+    )
 
     validCredentials = True
     if result is None:
@@ -77,22 +62,14 @@ def checkStaffCredentials(username, password):
 
 
 def addVisitor(firstName, lastName, residentName, date, phone, email):
-    connection = sqlite3.connect("springoak.db")
-    cursor = connection.cursor()
-    cursor.execute(
+    queryTheDabase(
         "INSERT INTO visitor (firstName, lastName, resident, date, phone, email) VALUES(?,?,?,?,?,?)",
-        (firstName, lastName, residentName, date, phone, email)
+        (firstName, lastName, residentName, date, phone, email),
     )
-    connection.commit()
-    connection.close()
 
 
 def removeVisitor(firstName, lastName, email):
-    connection = sqlite3.connect("springoak.db")
-    cursor = connection.cursor()
-    cursor.execute(
+    queryTheDabase(
         "DELETE FROM visitor WHERE FirstName = ? AND LastName = ? AND Email = ?",
         (firstName, lastName, email),
     )
-    connection.commit()
-    connection.close()
